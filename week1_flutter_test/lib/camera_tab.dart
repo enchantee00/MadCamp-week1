49c4cc1061848f231f73a34fcd85269012bf9933
jkookhan03 <jkookhan03@kaist.ac.kr>
2024-06-28 21:24:00 +0900

    contact_detail tap 추가 및 contacts tab 기능 추가

--- week1_flutter_test/lib/camera_tab.dart
+++ week1_flutter_test/lib/camera_tab.dart
@@ -7,37 +7,31 @@ import 'package:path/path.dart';
 
 class CameraTab extends StatefulWidget {
   final List<CameraDescription> cameras;
-  final Function(String) onPictureTaken; // 콜백 함수 추가
 
-  CameraTab({required this.cameras, required this.onPictureTaken});
+  CameraTab({required this.cameras});
 
   @override
   _CameraTabState createState() => _CameraTabState();
 }
 
 class _CameraTabState extends State<CameraTab> {
-  CameraController? controller;
-  Future<void>? initializeControllerFuture;
+  late CameraController controller;
+  late Future<void> initializeControllerFuture;
   String? imagePath;
 
-  void _initializeCamera() {
+  @override
+  void initState() {
+    super.initState();
     controller = CameraController(
-      widget.cameras[0],
+      widget.cameras[0], // widget.cameras로 변경합니다.
       ResolutionPreset.high,
     );
-    initializeControllerFuture = controller!.initialize();
-    setState(() {});
-  }
-
-  void _disposeCamera() {
-    controller?.dispose();
-    controller = null;
-    setState(() {});
+    initializeControllerFuture = controller.initialize();
   }
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
@@ -53,14 +47,12 @@ class _CameraTabState extends State<CameraTab> {
       );
 
       // Take the picture and save it to the given path
-      XFile picture = await controller!.takePicture();
+      XFile picture = await controller.takePicture();
       await picture.saveTo(imagePath);
 
       setState(() {
         this.imagePath = imagePath;
       });
-
-      widget.onPictureTaken(imagePath); // 콜백 호출하여 갤러리 탭에 전달
     } catch (e) {
       print(e);
     }
@@ -69,20 +61,13 @@ class _CameraTabState extends State<CameraTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: controller == null
-          ? Center(
-        child: ElevatedButton(
-          onPressed: _initializeCamera,
-          child: Text('Open Camera'),
-        ),
-      )
-          : FutureBuilder<void>(
+      body: FutureBuilder<void>(
         future: initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return Stack(
               children: [
-                CameraPreview(controller!),
+                CameraPreview(controller),
                 if (imagePath != null)
                   Positioned(
                     bottom: 10,
@@ -95,7 +80,7 @@ class _CameraTabState extends State<CameraTab> {
                   ),
                 Positioned(
                   bottom: 20,
-                  left: MediaQuery.of(context).size.width / 2 - 30, // 중앙에 위치하도록 설정
+                  left: MediaQuery.of(context).size.width / 2 - 50,
                   child: ElevatedButton(
                     onPressed: _takePicture,
                     child: Icon(Icons.camera_alt),
@@ -104,19 +89,6 @@ class _CameraTabState extends State<CameraTab> {
                       padding: EdgeInsets.all(20),
                       backgroundColor: Colors.blue, // Button color
                       foregroundColor: Colors.white, // Icon color
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 20, // 오른쪽 하단에 위치하도록 설정
-                  child: ElevatedButton(
-                    onPressed: _disposeCamera,
-                    child: Text('Close Camera'),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(16),
-                      backgroundColor: Colors.red, // Button color
-                      foregroundColor: Colors.white, // Text color
                     ),
                   ),
                 ),
--- week1_flutter_test/lib/gallery_tab.dart
+++ week1_flutter_test/lib/gallery_tab.dart
@@ -1,93 +1,12 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
-import 'package:intl/intl.dart';
 
-class GalleryTab extends StatefulWidget {
-  GalleryTab({Key? key}) : super(key: key);
-
-  @override
-  GalleryTabState createState() => GalleryTabState();
-}
-
-class GalleryTabState extends State<GalleryTab> {
-  List<File> images = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadImages();
-  }
-
-  void addImage(String path) {
-    setState(() {
-      images.add(File(path));
-      images.sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
-    });
-  }
-
-  Future<void> _loadImages() async {
-    final Directory appDir = await getApplicationDocumentsDirectory();
-    final String imageDir = join(appDir.path, 'Pictures');
-    final directory = Directory(imageDir);
-
-    if (await directory.exists()) {
-      final List<FileSystemEntity> files = directory.listSync();
-      setState(() {
-        images = files.where((file) => file.path.endsWith('.png')).map((file) => File(file.path)).toList();
-        images.sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
-      });
-    }
-  }
-
-  Map<String, List<File>> _groupByDate(List<File> images) {
-    Map<String, List<File>> groupedImages = {};
-    for (var image in images) {
-      String date = DateFormat('yyyy-MM-dd').format(image.lastModifiedSync());
-      if (groupedImages[date] == null) {
-        groupedImages[date] = [];
-      }
-      groupedImages[date]!.add(image);
-    }
-    return groupedImages;
-  }
-
+class GalleryTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Map<String, List<File>> groupedImages = _groupByDate(images);
-
-    return Scaffold(
-      body: groupedImages.isEmpty
-          ? Center(child: Text('No images found.'))
-          : ListView(
-        children: groupedImages.keys.map((String date) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  date,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
-                ),
-                itemCount: groupedImages[date]!.length,
-                itemBuilder: (context, index) {
-                  return Image.file(groupedImages[date]![index]);
-                },
-              ),
-            ],
-          );
-        }).toList(),
+    return Center(
+      child: Text(
+        'Gallery',
+        style: TextStyle(fontSize: 24),
       ),
     );
   }
